05c9cb99df61ff9c3b42c3bd8c45ecb4c6eb4628
Pravesh Kumar <161683443+Pravesh2201@users.noreply.github.com>
2024-08-28 23:43:20 +0530

    Update notification.groovy

--- vars/notification.groovy
+++ vars/notification.groovy
@@ -1,6 +1,6 @@
 def call() {
     stage('notify') {
           emailext body: 'success', subject: 'Status', to: 'praveshgangwar04@gmail.com'
-          slackSend channel: '#jenkins-notification', message: 'successfull '
+          slackSend channel: '#jenkins-notification', message: "Build Successful: ${env.JOB_NAME} [${env.BUILD_NUMBER}]"
     }
 }
